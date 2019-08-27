9b4d416eca49c06ea4f77bf9dc36b75b6439679b
Xidorn Quan <me@upsuper.org>
2019-08-27 10:26:16 +1000

    Avoid triggering cancel / end when a gesture isn't recognized

--- src/recognizerjs/state-str.js
+++ src/recognizerjs/state-str.js
@@ -12,14 +12,16 @@ import {
  * @returns {String} state
  */
 export default function stateStr(state) {
-  if (state & STATE_CANCELLED) {
-    return 'cancel';
-  } else if (state & STATE_ENDED) {
-    return 'end';
-  } else if (state & STATE_CHANGED) {
-    return 'move';
-  } else if (state & STATE_BEGAN) {
-    return 'start';
+  if (state & (STATE_BEGAN | STATE_CHANGED)) {
+    if (state & STATE_CANCELLED) {
+      return 'cancel';
+    } else if (state & STATE_ENDED) {
+      return 'end';
+    } else if (state & STATE_CHANGED) {
+      return 'move';
+    } else if (state & STATE_BEGAN) {
+      return 'start';
+    }
   }
   return '';
 }
